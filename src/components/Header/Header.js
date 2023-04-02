9ea641290ebf8e22b6f6abee2ef30c25a4433f91
Vipul Malhotra <vipulmalhotra@192.168.1.6>
2023-04-02 14:39:45 +0530

    responsiveness added to the site

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,13 +12,16 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import {Ic} from 'react-icons/fa';
 import './Header.css';
 
 const pages = ['Products', 'Pricing', 'Blog'];
-const pages2 = ['Enna', 'Meena', 'Dika'];
-
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const socialMedia = [
+  {key:"git", hrefValue:"https://github.com/vipulm124", classValue:"fa fa-brands fa-github social-icon"},
+  {key:"li", hrefValue:"https://www.linkedin.com/in/vipul-malhotra-5545b829/", classValue:"fa fa-brands fa-linkedin social-icon"},
+  {key:"email", hrefValue:"mailto:vipulm124@gmail.com", classValue:"fa fa-solid fa-envelope social-icon"}
+]
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -43,7 +46,7 @@ function ResponsiveAppBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-
+       
           <Typography
             variant="h6"
             noWrap
@@ -52,18 +55,55 @@ function ResponsiveAppBar() {
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
-              
               fontWeight: 700,
               letterSpacing: '.3rem',
               color: 'inherit',
               textDecoration: 'none',
+              fontSize: '1.5rem'
             }}
           >
             <span className='first-name'>VIPUL</span> MALHOTRA
           </Typography>
 
-         
-   
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }} style={{marginLeft:20}}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {socialMedia.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <a  href={page.hrefValue} target="_blank"> 
+                    <i className={page.classValue}></i>
+            </a>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+    
           <Typography
             variant="h5"
             noWrap
@@ -73,51 +113,26 @@ function ResponsiveAppBar() {
               mr: 2,
               display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
-              fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
               color: 'inherit',
               textDecoration: 'none',
+              fontSize: '1.5rem'
             }}
           >
             <span className='first-name'>VIPUL</span> MALHOTRA
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} style={{marginLeft:20}}>
-          </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-          <a  href="https://www.linkedin.com/in/vipul-malhotra-5545b829/" target="_blank">
-           <i className="fa fa-brands fa-linkedin social-icon"></i>
-            </a>
-            <a  href="https://github.com/vipulm124" target="_blank">
-           <i className="fa fa-brands fa-github social-icon"></i>
-            </a>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} style={{flexDirection:'row-reverse'}}>
             <a  href="mailto:vipulm124@gmail.com">
            <i className="fa fa-solid fa-envelope social-icon"></i>
             </a>
-            
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+ <a  href="https://www.linkedin.com/in/vipul-malhotra-5545b829/" target="_blank">
+           <i className="fa fa-brands fa-linkedin social-icon"></i>
+            </a>
+ <a  href="https://github.com/vipulm124" target="_blank">
+           <i className="fa fa-brands fa-github social-icon"></i>
+            </a>
           </Box>
         </Toolbar>
       </Container>
--- src/components/Hello/Hello.js
+++ src/components/Hello/Hello.js
@@ -2,51 +2,49 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid';
 import './Hello.css';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
+  padding: theme.spacing(4),
+  textAlign: 'left',
   color: theme.palette.text.secondary,
 }));
 
 export default function Hello() {
   return (
     <div className='container'>
-
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-       
-      <Grid item xs={6} md={6} justifyItems="right" justifyContent='end' justifySelf='right'>
-          <div style={{marginLeft:'30%'}}>
+     <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        justifyContent="flex-start"
+        alignItems="left"
+        spacing={8}
+      >
+        <Item className='stack-item' >
+        <div >
             <p className='hello-p'>Hello, I am</p>
             <p className='hello-name'>&lt; Vipul</p>
-            <p className='hello-name hello-surname'>Malhotra /&gt;</p>
-            <p className='hello-surname'>Azure Cloud Developer</p>
-            <p className='hello-surname'>Fullstack Developer</p>
-            <p className='hello-surname'>React Native Developer</p>
-            
-
-
-          </div>
-          
-        </Grid>
-        <Grid item xs={6} md={6}>
-          <div style={{marginRight:'70%'}}>
+            <p className='hello-name hello-surname'>Malhotra / &gt;</p>
+            <p className='hello-stack'>Azure Cloud Developer</p>
+            <p className='hello-stack'>Fullstack Developer</p>
+            <p className='hello-stack'>React Native Developer</p>
+        </div>
+        </Item>
+        <Item className='stack-item'>
+        <div >
             <img src='https://media.licdn.com/dms/image/C4D03AQEyUCzU1GTYkg/profile-displayphoto-shrink_800_800/0/1648457056150?e=1685577600&v=beta&t=sP6OK4LOOv0uLz_NzWeKed5vqQEElspwL7vM4TwDSLA' alt='Profile Pic' className='profile-pic'/>
             <div>
               <span className='yr-count'>10</span>
               <span className='yr-text'>YEARS OF EXPERIENCE</span>
             </div>
-          </div>
+            </div>
+        </Item>
 
-        </Grid>
-      
-      </Grid>
-    </Box>
+      </Stack>
+
     </div>
 
   );
